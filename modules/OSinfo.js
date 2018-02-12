c9d48d8df3f01e3005e37e0b42cd8e734274cc89
Rafał Kowalczyk <rako.r.kowalczyk@interia.pl>
2018-02-12 10:53:17 +0100

    Add code in modules/OSinfo.js

--- modules/OSinfo.js
+++ modules/OSinfo.js
@@ -1,1 +1,25 @@
-﻿
+﻿var os = require('os');
+var OStime = require('./OStime');
+
+function getOSinfo() {
+	var type = os.type();	
+	if(type === 'Darwin') {
+		type = "OSX";
+	} else if (type === 'Windows NT') {
+		type = 'Windows';
+	}
+	var release = os.release();
+	var cpu = os.cpus()[0].model;
+	var userInfo = os.userInfo();
+	var uptime = OStime.print();
+	
+	console.log('System: ', type);
+	console.log('Release: ', release);
+	console.log('CPU model: ', cpu);
+	console.log('User name: ', userInfo.username);
+	console.log('Home dir: ', userInfo.homedir);
+}
+
+
+
+exports.print = getOSinfo;
