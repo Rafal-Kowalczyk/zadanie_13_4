5e1ae8a29be3c7f53aed76cb4ffa7b164b153418
Rafał Kowalczyk <rako.r.kowalczyk@interia.pl>
2018-02-12 10:55:21 +0100

    Add code modules/OStime.js

--- modules/OStime.js
+++ modules/OStime.js
@@ -1,1 +1,19 @@
-﻿
+﻿var os = require('os');
+
+function getOStime() {
+	var uptime = os.uptime().toFixed(0);
+
+	var minutes = Math.floor(uptime / 60).toFixed(0);
+	var restMinutes = (uptime % 60);
+
+	var hours = Math.floor(uptime / 3600);		
+	var restHuor = (minutes % 60).toFixed(0);
+
+	console.log('Uptime in secends: ~ ' + uptime + " s");	
+	console.log('Uptime in minutes: ~ ' + minutes + ' min ' + restMinutes+ ' s');
+	console.log('Uptime in hours: ~ '+ hours + " h " + restHuor + ' min ' + restMinutes + " s");
+
+}
+
+
+exports.print = getOStime;
